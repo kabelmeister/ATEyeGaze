a469e3079dc870409ddf21af97078afb301a1a82
pavsec <34980273+pavsec@users.noreply.github.com>
2021-01-09 14:41:45 +0100

    Added buttons with bg image

---
+++ Assets/Scripts/AddButtons.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddButtons : MonoBehaviour
+{
+    [SerializeField] private Transform field;
+
+    [SerializeField] private GameObject btn;
+
+    public int btn_cnt = 8;
+
+    void Awake ()
+    {
+        for (int i = 0; i < btn_cnt; i++)
+        {
+            GameObject button = Instantiate(btn);
+            button.name = "" + i;
+            button.transform.SetParent(field, false);
+        }            
+    }
+}
---
+++ Assets/Scripts/GameControlMemory.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GameControlMemory : MonoBehaviour
+{
+    [SerializeField] private Sprite bgImage;
+
+    public List<Button> btns = new List<Button>();
+
+    void Start()
+    {
+        GetButtons();
+    }
+
+    void GetButtons()
+    {
+        GameObject[] objects = GameObject.FindGameObjectsWithTag("PuzzleButton");
+
+        for (int i = 0; i < objects.Length; i++)
+        {
+            btns.Add(objects[i].GetComponent<Button>());
+            btns[i].image.sprite = bgImage;
+        }
+    }
+
+
+    public void PickAPuzzle()
+    {
+        Debug.Log("btn click");
+    }
+
+}
